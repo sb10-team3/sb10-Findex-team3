ec4ffde377a04e95ad3bf441c86657b9c35c64b1
초시맥북 <fyg7575@gmail.com>
2026-03-11 17:36:27 +0900

    프로젝트 초기 설정

---
+++ src/test/java/org/codeiteam3/findex/FindexApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codeiteam3.findex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
