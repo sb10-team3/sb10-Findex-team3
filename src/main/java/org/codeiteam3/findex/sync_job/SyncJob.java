9802aff496026e123fb9b6b4a0eb551b9f19e08d
초시맥북 <fyg7575@gmail.com>
2026-03-12 16:10:49 +0900

    refactor: 필요없는 import 정리

--- src/main/java/org/codeiteam3/findex/sync_job/SyncJob.java
+++ src/main/java/org/codeiteam3/findex/sync_job/SyncJob.java
@@ -3,7 +3,6 @@ package org.codeiteam3.findex.sync_job;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
