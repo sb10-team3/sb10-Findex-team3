7b92df5962a6ccb49bcb1ced836e47e3d80831ff
초시맥북 <fyg7575@gmail.com>
2026-03-12 16:09:09 +0900

    fix: NoArgsConstructor의 파라미터에서 AccessLevel 삭제

--- src/main/java/org/codeiteam3/findex/sync_job/SyncJob.java
+++ src/main/java/org/codeiteam3/findex/sync_job/SyncJob.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 import java.time.Instant;
 import java.util.UUID;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 @Getter
 @Entity
 @Table(name = "sync_jobs")
