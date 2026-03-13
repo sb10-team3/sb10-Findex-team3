ea6c65b648f01c27bde161278356a4823beda3e5
초시맥북 <fyg7575@gmail.com>
2026-03-13 20:02:16 +0900

    refactor: dto에 data와 연결되는 필드 표기

--- src/main/java/org/codeiteam3/findex/sync_job/dto/IndexApiResponseItemDto.java
+++ src/main/java/org/codeiteam3/findex/sync_job/dto/IndexApiResponseItemDto.java
@@ -1,19 +1,19 @@
 package org.codeiteam3.findex.sync_job.dto;
 
 public record IndexApiResponseItemDto(
-        String basDt,            // 기준일자 info
+        String basDt,            // 기준일자 info data
         String idxNm,            // 지수명 info
         String idxCsf,           // 지수 분류 info
         String epyItmsCnt,       // 편입 종목 수 info
-        String clpr,             // 종가
-        String vs,               // 전일 대비
-        String fltRt,            // 등락률
-        String mkp,              // 시가
-        String hipr,             // 고가
-        String lopr,             // 저가
-        String trqu,             // 거래량
-        String trPrc,            // 거래대금
-        String lstgMrktTotAmt,   // 상장 시가총액
+        String clpr,             // 종가 data
+        String vs,               // 전일 대비 data
+        String fltRt,            // 등락률 data
+        String mkp,              // 시가 data
+        String hipr,             // 고가 data
+        String lopr,             // 저가 data
+        String trqu,             // 거래량 data
+        String trPrc,            // 거래대금 data
+        String lstgMrktTotAmt,   // 상장 시가총액 data
         String lsYrEdVsFltRg,    // 연초 대비 등락폭
         String lsYrEdVsFltRt,    // 연초 대비 등락률
         String yrWRcrdHgst,      // 연중 최고가
