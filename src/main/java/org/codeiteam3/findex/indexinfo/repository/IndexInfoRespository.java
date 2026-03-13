732e17a5ac32840c91c70a11572046d5b8628d25
kwakinsung <kis2690@gmail.com>
2026-03-13 13:43:25 +0900

    feat: 지수 정보 등록 Repository계층설계

---
+++ src/main/java/org/codeiteam3/findex/indexinfo/repository/IndexInfoRespository.java
@@ -0,0 +1,29 @@
+package org.codeiteam3.findex.indexinfo.repository;
+
+import org.codeiteam3.findex.SourceType;
+import org.codeiteam3.findex.indexinfo.entity.IndexInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface IndexInfoRespository extends JpaRepository<IndexInfo, UUID> {
+
+
+    boolean existsByIndexClassificationAndIndexName( //지수분류 + 지수 이름으로 존재 검증
+            String indexClassification,
+            String indexName
+    );
+
+    boolean existsByIndexClassificationAndIndexNameAndSourceType(
+            String indexClassification,
+            String indexName,
+            SourceType sourceType
+    );
+
+    IndexInfo findByIndexClassificationAndIndexNameAndSourceType(
+            String indexClassification,
+            String indexName,
+            SourceType sourceType
+    );
+
+}
