87b846bed93ba50aa9e2c8fc35d1399b4cba76f7
kwakinsung <kis2690@gmail.com>
2026-03-13 14:07:58 +0900

    refactor: 지수 정보 등록 Controller mapper 적용

--- src/main/java/org/codeiteam3/findex/indexinfo/controller/IndexInfoController.java
+++ src/main/java/org/codeiteam3/findex/indexinfo/controller/IndexInfoController.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.codeiteam3.findex.indexinfo.dto.data.IndexInfoDto;
 import org.codeiteam3.findex.indexinfo.dto.request.IndexInfoCreateRequest;
 import org.codeiteam3.findex.indexinfo.entity.IndexInfo;
+import org.codeiteam3.findex.indexinfo.mapper.IndexInfoMapper;
 import org.codeiteam3.findex.indexinfo.service.IndexInfoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,26 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class IndexInfoController {
     private final IndexInfoService indexInfoService;
+    private final IndexInfoMapper indexInfoMapper;
 
     @PostMapping
     public ResponseEntity<IndexInfoDto> create(@RequestBody IndexInfoCreateRequest indexInfoCreateRequest) {
         IndexInfo indexInfo = indexInfoService.create(indexInfoCreateRequest);
 
-        IndexInfoDto indexInfoDto = new IndexInfoDto(
-                indexInfo.getId(),
-                indexInfo.getIndexClassification(),
-                indexInfo.getIndexName(),
-                indexInfo.getEmployedItemsCount(),
-                indexInfo.getBasePointInTime(),
-                indexInfo.getBaseIndex(),
-                indexInfo.getSourceType(),
-                indexInfo.getFavorite()
-        );
-
-        return ResponseEntity.ok(indexInfoDto);
-
-
-
+        IndexInfoDto indexInfoDto = indexInfoMapper.toDto(indexInfo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(indexInfoDto);
 
     }
 }
