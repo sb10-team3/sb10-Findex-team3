3fb56edfcfec4c2543b825804d98b8d834376c4e
kwakinsung <kis2690@gmail.com>
2026-03-13 15:45:50 +0900

    refactor: AutoSyncConfig Repository 패키지화 전으로 변경

--- src/main/java/org/codeiteam3/findex/autosyncconfig/repository/AutoSyncConfigRepository.java
+++ src/main/java/org/codeiteam3/findex/autosyncconfig/repository/AutoSyncConfigRepository.java
@@ -1,6 +1,6 @@
 package org.codeiteam3.findex.autosyncconfig.repository;
 
-import org.codeiteam3.findex.autosyncconfig.entity.AutoSyncConfig;
+import org.codeiteam3.findex.autosyncconfig.AutoSyncConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
