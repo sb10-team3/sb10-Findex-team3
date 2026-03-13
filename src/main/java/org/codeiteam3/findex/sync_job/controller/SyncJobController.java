7c1bbfcde7fbd5a342573628d8de6a9da34c2760
초시맥북 <fyg7575@gmail.com>
2026-03-13 15:25:14 +0900

    feat: 요청자의 IP 주소를 받아오기 위해 파라미터로 HttpServletRequest 타입을 받도록 수정함.

--- src/main/java/org/codeiteam3/findex/sync_job/controller/SyncJobController.java
+++ src/main/java/org/codeiteam3/findex/sync_job/controller/SyncJobController.java
@@ -1,17 +1,23 @@
 package org.codeiteam3.findex.sync_job.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.codeiteam3.findex.sync_job.dto.SyncJobDto;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.codeiteam3.findex.sync_job.service.SyncJobService;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/sync-jobs")
 public class SyncJobController {
-    @GetMapping("/index-infos")
-    public SyncJobDto getIndexInfos() {
-        return null;
+    private final SyncJobService syncJobService;
+
+    @PostMapping("/index-infos")
+    public List<SyncJobDto> createIndexInfoSyncJob(HttpServletRequest request) {
+        return syncJobService.indexInfoSyncJob(request.getRemoteAddr());
     }
 }
