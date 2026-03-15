2caf069a80423aabff9b61b73a14e07ef25b3faa
JungH200000 <jhpaxk0604@gmail.com>
2026-03-15 11:18:19 +0900

    feat: `Repository`에서 `marketPrice`, `closingPrice`, `highPrice`, `lowPrice`, `versus`, `fluctuationRate` 기준으로 지수 데이터를 정렬 조회하는 로직 구현

--- src/main/java/org/codeiteam3/findex/indexdata/repository/IndexDataRepository.java
+++ src/main/java/org/codeiteam3/findex/indexdata/repository/IndexDataRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
@@ -45,6 +46,66 @@ public interface IndexDataRepository extends JpaRepository<IndexData, UUID> {
             @Param("endDate") LocalDate endDate,
             @Param("idAfter") UUID idAfter,
             @Param("cursor") LocalDate normalizedCursor,
+            Pageable pageable
+    );
+
+    // 정렬 기준이 BigDecimal일 때
+    @Query("SELECT i FROM IndexData AS i " +
+            "WHERE (:indexInfoId IS NULL OR i.indexInfo.id = :indexInfoId) " +
+            "   AND (:startDate IS NULL OR i.baseDate >= :startDate) " +
+            "   AND (:endDate IS NULL OR i.baseDate <= :endDate) " +
+            "   OR CASE " +
+            "       WHEN :sortField = 'marketPrice' THEN i.marketPrice " +
+            "       WHEN :sortField = 'closingPrice' THEN i.closingPrice " +
+            "       WHEN :sortField = 'highPrice' THEN i.highPrice " +
+            "       WHEN :sortField = 'lowPrice' THEN i.lowPrice " +
+            "       WHEN :sortField = 'versus' THEN i.versus " +
+            "       WHEN :sortField = 'fluctuationRate' THEN i.fluctuationRate " +
+            "   END < :cursor" +
+            "   OR (CASE " +
+            "       WHEN :sortField = 'marketPrice' THEN i.marketPrice " +
+            "       WHEN :sortField = 'closingPrice' THEN i.closingPrice " +
+            "       WHEN :sortField = 'highPrice' THEN i.highPrice " +
+            "       WHEN :sortField = 'lowPrice' THEN i.lowPrice " +
+            "       WHEN :sortField = 'versus' THEN i.versus " +
+            "       WHEN :sortField = 'fluctuationRate' THEN i.fluctuationRate " +
+            "   END = :cursor AND i.id > :idAfter)")
+    Slice<IndexData> findAllByBigDecimalCursorDesc(
+            @Param("indexInfoId") UUID indexInfoId,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            @Param("isAfter") UUID isAfter,
+            @Param("cursor") BigDecimal normalizedCursor,
+            @Param("sortField") String normalizedSortField,
+            Pageable pageable
+    );
+    @Query("SELECT i FROM IndexData AS i " +
+            "WHERE (:indexInfoId IS NULL OR i.indexInfo.id = :indexInfoId) " +
+            "   AND (:startDate IS NULL OR i.baseDate >= :startDate) " +
+            "   AND (:endDate IS NULL OR i.baseDate <= :endDate) " +
+            "   OR CASE " +
+            "       WHEN :sortField = 'marketPrice' THEN i.marketPrice " +
+            "       WHEN :sortField = 'closingPrice' THEN i.closingPrice " +
+            "       WHEN :sortField = 'highPrice' THEN i.highPrice " +
+            "       WHEN :sortField = 'lowPrice' THEN i.lowPrice " +
+            "       WHEN :sortField = 'versus' THEN i.versus " +
+            "       WHEN :sortField = 'fluctuationRate' THEN i.fluctuationRate " +
+            "   END > :cursor" +
+            "   OR (CASE " +
+            "       WHEN :sortField = 'marketPrice' THEN i.marketPrice " +
+            "       WHEN :sortField = 'closingPrice' THEN i.closingPrice " +
+            "       WHEN :sortField = 'highPrice' THEN i.highPrice " +
+            "       WHEN :sortField = 'lowPrice' THEN i.lowPrice " +
+            "       WHEN :sortField = 'versus' THEN i.versus " +
+            "       WHEN :sortField = 'fluctuationRate' THEN i.fluctuationRate " +
+            "   END = :cursor AND i.id < :idAfter)")
+    Slice<IndexData> findAllByBigDecimalCursorAsc(
+            @Param("indexInfoId") UUID indexInfoId,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            @Param("isAfter") UUID isAfter,
+            @Param("cursor") BigDecimal normalizedCursor,
+            @Param("sortField") String normalizedSortField,
             Pageable pageable
     );
 
