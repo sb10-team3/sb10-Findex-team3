a7c84f04858edb7f495a293b703df0cb72be6490
zelote123 <fredsonata@gmail.com>
2026-03-15 21:14:35 +0900

    feat: 지수 성과 랭킹 조회 레포지토리 메서드 구현

--- src/main/java/org/codeiteam3/findex/indexdata/repository/IndexDataRepository.java
+++ src/main/java/org/codeiteam3/findex/indexdata/repository/IndexDataRepository.java
@@ -2,6 +2,8 @@ package org.codeiteam3.findex.indexdata.repository;
 
 import org.codeiteam3.findex.indexdata.entity.IndexData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -18,6 +20,21 @@ public interface IndexDataRepository extends JpaRepository<IndexData, UUID> {
 
 
 
-    // 대쉬보드 메서드
+
     List<IndexData> findByIndexInfoIdOrderByBaseDate(UUID indexInfoId);
+    // 특정 지수 조회 시
+    // 특정 지수를 기준일로 이하로 내림차순으로 정렬했을때 가장 최신의 지수 데이터를 조회
+    List<IndexData> findTop1ByIndexInfoIdAndBaseDateLessThanEqualOrderByBaseDateDesc(UUID indexInfoId, LocalDate baseDateIsLessThan);
+    // 전체 지수 조회 시
+    // 각 지수별로 타겟 날짜 이전의 가장 최신 날짜를 찾은 뒤, 원본 테이블과 조인해서 데이터를 가져옴
+    @Query(value =
+            "SELECT d.* FROM index_datas d " +
+                    "INNER JOIN (" +
+                    "    SELECT index_info_id, MAX(base_date) AS max_date " +
+                    "    FROM index_datas " +
+                    "    WHERE base_date <= :targetDate " +
+                    "    GROUP BY index_info_id" +
+                    ") sub ON d.index_info_id = sub.index_info_id AND d.base_date = sub.max_date",
+            nativeQuery = true)
+    List<IndexData> findLatestDataOfAllIndexesOnOrBefore(@Param("targetDate") LocalDate targetDate);
 }
