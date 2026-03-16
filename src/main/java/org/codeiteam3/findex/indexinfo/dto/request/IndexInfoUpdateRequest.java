bbb07f4a099c0015d70733b682dd9d745cc884f8
kwakinsung <kis2690@gmail.com>
2026-03-16 18:27:17 +0900

    feat: 지수정보 수정기능 요청 DTO 설계

---
+++ src/main/java/org/codeiteam3/findex/indexinfo/dto/request/IndexInfoUpdateRequest.java
@@ -0,0 +1,23 @@
+package org.codeiteam3.findex.indexinfo.dto.request;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record IndexInfoUpdateRequest(
+
+        Integer employedItemsCount,
+
+        LocalDate basePointInTime,
+
+        @DecimalMin(value = "0.0", inclusive = false, message = "기준 지수는 0보다 커야 합니다.")
+        @Digits(integer = 16, fraction = 2, message = "기준 지수 형식이 올바르지 않습니다.")
+        BigDecimal baseIndex,
+
+        Boolean favorite
+) {
+}
