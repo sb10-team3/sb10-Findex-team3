4c9917ab0253d363f4fae9b7368911974b214600
초시맥북 <fyg7575@gmail.com>
2026-03-16 10:59:06 +0900

    fix: 서비스 메서드에 트랜잭션 적용

--- src/main/java/org/codeiteam3/findex/autosyncconfig/service/AutoSyncConfigService.java
+++ src/main/java/org/codeiteam3/findex/autosyncconfig/service/AutoSyncConfigService.java
@@ -1,14 +1,17 @@
 package org.codeiteam3.findex.autosyncconfig.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.codeiteam3.findex.autosyncconfig.AutoSyncConfig;
 import org.codeiteam3.findex.autosyncconfig.dto.AutoSyncConfigResponseDto;
 import org.codeiteam3.findex.autosyncconfig.dto.AutoSyncConfigUpdateRequestDto;
 import org.codeiteam3.findex.autosyncconfig.mapper.AutoSyncConfigMapper;
 import org.codeiteam3.findex.autosyncconfig.repository.AutoSyncConfigRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -17,13 +20,14 @@ public class AutoSyncConfigService {
     private final AutoSyncConfigRepository autoSyncConfigRepository;
     private final AutoSyncConfigMapper autoSyncConfigMapper;
 
+    @Transactional
     public AutoSyncConfigResponseDto update(UUID id, AutoSyncConfigUpdateRequestDto dto) {
         AutoSyncConfig config = autoSyncConfigRepository.findByIdWithIndexInfo(id)
                 .orElseThrow(NoSuchElementException::new);
 
         config.update(dto.enabled());
 
-        return autoSyncConfigMapper.toDto(config);
+        return autoSyncConfigMapper.toDto(autoSyncConfigRepository.save(config));
     }
 
 }
