7c94729fe50a7f5a0956698d2df705fdc2759a17
초시맥북 <fyg7575@gmail.com>
2026-03-16 10:21:27 +0900

    fix: indexinfo 연동 시 없는 데이터를 받아올 경우 자동으로 자동연동 설정 객체가 생성되도록

--- src/main/java/org/codeiteam3/findex/autosyncconfig/repository/AutoSyncConfigRepository.java
+++ src/main/java/org/codeiteam3/findex/autosyncconfig/repository/AutoSyncConfigRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface AutoSyncConfigRepository extends JpaRepository<AutoSyncConfig,UUID> {
+    AutoSyncConfig findByIndexInfoId(UUID indexInfoId);
 }
