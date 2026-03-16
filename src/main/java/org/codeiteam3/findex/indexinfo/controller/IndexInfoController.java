c44ca58350f9db87ca343d81bc4af678142ee17e
kwakinsung <kis2690@gmail.com>
2026-03-16 09:42:23 +0900

    feat: 지수정보 다건조회 API 설계

--- src/main/java/org/codeiteam3/findex/indexinfo/controller/IndexInfoController.java
+++ src/main/java/org/codeiteam3/findex/indexinfo/controller/IndexInfoController.java
@@ -1,5 +1,8 @@
 package org.codeiteam3.findex.indexinfo.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.codeiteam3.findex.common.CursorPageResponse;
@@ -28,6 +31,21 @@ public class IndexInfoController {
         IndexInfoDto indexInfoDto = indexInfoMapper.toDto(indexInfo);
         return ResponseEntity.status(HttpStatus.CREATED).body(indexInfoDto);
 
+    }
+    @GetMapping
+    @Operation(summary = "지수 정보 목록 조회",description = "지수 정보 목록을 조회합니다. 필터링,정렬,커서 기반 페이지네이션을 지원합니다.")
+    public ResponseEntity<CursorPageResponseIndexInfoDto> findAll(@Parameter(description = "지수 분류명") @RequestParam String indexClassification,
+                                                                  @Parameter(description = "지수명") @RequestParam String indexName,
+                                                                  @Parameter(description = "즐겨찾기 여부") @RequestParam boolean favorite,
+                                                                  @Parameter(description = "이전 페이지 마지막 요소 ID") @RequestParam Integer idAfter,
+                                                                  @Parameter(description = "커서 (다음 페이지 시작점)") @RequestParam String cursor,
+                                                                  @Parameter(description = "정렬필드(indexClassification,indexName,employedItemsCount)") @RequestParam String sortField,
+                                                                  @Parameter(description = "정렬방향(asc,desc)") @RequestParam String sortDirection,
+                                                                  @Parameter(description = "페이지 크기") @RequestParam Integer size
+
+                                                                  ) {
+        CursorPageResponseIndexInfoDto responseDto = indexInfoService.findAll(indexClassification,indexName,favorite,idAfter,cursor,sortField,sortDirection,size);
+        return ResponseEntity.ok(responseDto);
     }
 
     @GetMapping("/{id}")
