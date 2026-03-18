84e3c7411ab2209ffc6a6964864e9feae97bc301
kwakinsung <kis2690@gmail.com>
2026-03-18 11:32:56 +0900

    feat: QueryDSL Config

---
+++ src/main/java/org/codeiteam3/findex/config/QueryDslConfig.java
@@ -0,0 +1,20 @@
+package org.codeiteam3.findex.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+// `private final JPAQueryFactory queryFactory` 문제 해결을 위한 bean 등록
+public class QueryDslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
